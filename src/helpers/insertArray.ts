259afc53e14d333e6da0e35352c907700293313c
longjuan <longjuan.xy@raycloud.com>
2022-04-01 19:16:05 +0800

    清空

--- src/helpers/insertArray.ts
+++
@@ -1,9 +0,0 @@
-export function insertArray<Item>(
-  array: Array<Item>,
-  asIndex: number,
-  item: Item
-) {
-  const newArr = [...array];
-  newArr.splice(asIndex, 0, item);
-  return newArr;
-}
